19c70329ca357f84747c6cad9e7cb9aebd35e173
Thorsten Sommer <service@sommer-engineering.com>
2017-04-03 21:14:45 +0200

    Init

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM golang
+
+# Update the operating system and install base tools:
+RUN \
+	apt-get update && \
+	apt-get upgrade -y && \
+	apt-get install -y zip
+
+# Insert all files from the repo (but from the current directory, not from Git):
+ADD . /go/src/github.com/SommerEngineering/WebProxy/
+
+# Compile and Setup
+RUN	cd /go/src/github.com/SommerEngineering/WebProxy && \
+
+	# Compile the code:
+	go install && \
+
+	# Copy the final binary and the runtime scripts to the home folder:
+	cp /go/bin/WebProxy /home && \
+
+	# Uninstall tools:
+	apt-get autoremove -y zip && \
+
+	# Delete the entire Go workspace:
+	rm -r -f /go && \
+
+	# Make the scripts executable:
+	chmod 0777 /home/WebProxy
+
+# Run anything below as nobody:
+USER nobody
+
+# Service provides HTTP by port 80:
+EXPOSE 80
+
+ENV CONFIGURATION="my-domain => http://www.another-domain.com"
+
+# Define the working directory:
+WORKDIR /home
+
+# The default command to run, if a container starts:
+CMD ["/home/WebProxy", "echo ${CONFIGURATION}"]
---
+++ Main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	proxyHosts        []string          = make([]string, 10)
+	proxyDestinations map[string]string = make(map[string]string)
+)
+
+func main() {
+
+	// Print the version:
+	log.Println(`WebProxy v1.0.0`)
+
+	// Parse all configurations:
+	for _, arg := range os.Args[1:] {
+		elements := strings.Split(arg, ` => `)
+		proxyHosts = append(proxyHosts, elements[0])
+		proxyDestinations[elements[0]] = elements[1]
+	}
+
+	// Wire the handlers:
+	serverMUX := http.NewServeMux()
+	serverMUX.HandleFunc("/", proxy)
+
+	// Set up the server:
+	server := &http.Server{}
+	server.Addr = ":80"
+	server.Handler = serverMUX
+	server.SetKeepAlivesEnabled(true)
+	server.ReadTimeout = 60 * time.Second
+	server.WriteTimeout = 60 * time.Second
+
+	log.Println(`Proxy ready.`)
+	server.ListenAndServe()
+}
+
+func proxy(response http.ResponseWriter, request *http.Request) {
+
+	// Which host was requested?
+	requestedHost := request.Host
+
+	// Get all parameters:
+	uri := request.RequestURI
+
+	// Loop over all configured hosts:
+	for _, host := range proxyHosts {
+
+		// Known?
+		if strings.Contains(requestedHost, host) {
+
+			// Read the destination:
+			destination := proxyDestinations[host]
+
+			// Create a client in order to connect both:
+			client := &http.Client{}
+
+			// Create the client request:
+			if clientRequest, clientErr := http.NewRequest(request.Method, destination+uri, request.Body); clientErr != nil {
+				log.Printf("Was not able to create connection to destination '%s' for host '%s': %s\n", destination, requestedHost, clientErr.Error())
+				http.NotFound(response, request)
+				return
+			} else {
+				// Perform the request:
+				if clientResponse, clientErrDo := client.Do(clientRequest); clientErrDo != nil {
+					log.Printf("Was not able to perform the proxy request to destination '%s' for host '%s': %s\n", destination, requestedHost, clientErrDo.Error())
+					http.NotFound(response, request)
+					return
+				} else {
+					defer clientResponse.Body.Close()
+					io.Copy(response, clientResponse.Body)
+					return
+				}
+			}
+
+			// Break the loop:
+			return
+		}
+	}
+
+	// The requested host is unknown:
+	log.Printf("Host '%s' was not configured.\n", requestedHost)
+	http.NotFound(response, request)
+	return
+}
