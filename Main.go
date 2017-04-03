04e50fb5efaaa99f28262d16b27b5ec0a57de5b9
Thorsten Sommer <service@sommer-engineering.com>
2017-04-03 21:33:25 +0200

    Fixed len operator

--- Main.go
+++ Main.go
@@ -24,7 +24,7 @@ func main() {
 	for _, arg := range os.Args[1:] {
 		elements := strings.Split(arg, ` => `)
 
-		if len(elements != 2) {
+		if len(elements) != 2 {
 			log.Printf("The configuration '%s' uses a invalid format. Necessary format is: 'my-domain => http://www.other-domain.com'.\n", arg)
 			continue
 		}
